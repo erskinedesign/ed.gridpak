665b2ac732c5359917403e69cf40322047e28f13
Wil Linssen <wil@erskinedesign.com>
2011-08-18 18:30:56 +0100

    drawing cols

--- static/js/onload.js
+++ static/js/onload.js
@@ -1,13 +1,33 @@
-$(document).ready(function(){
-    var $browser = $('div#browser'),
-        $info = $('div#info'),
-        grid = 50;
+var $browser = {},
+        $info = {},
+        options = {
+            snap: 50,
+            cols: 10
+        };
 
+$(document).ready(function(){
+
+    $browser = $('div#browser');
+    $info = $('div#info');
+    draw_cols();
+        
     $browser.resizable({
-        grid: grid,
+        grid: options.snap,
         resize: function(event, ui) {
-            width = Math.round(ui.size.width / grid) * 50;
+            width = Math.round(ui.size.width / options.snap) * 50;
             $info.html(width + 'px');
         }
     });
 });
+
+
+function draw_cols() {
+
+    var col = '<div class="col" />',
+        i = 0;
+
+    for (i; i<=options.cols; i++) {
+        $browser.append($(col));
+    }
+
+}
