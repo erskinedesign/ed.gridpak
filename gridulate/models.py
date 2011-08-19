e98503fe35bfb3a375863ce26183b361166891e8
Wil Linssen <wil@erskinedesign.com>
2011-08-19 14:16:11 +0100

    model for grid

--- gridulate/models.py
+++ gridulate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Grid(models.Model):
+    lowest_width = models.IntegerField()
+    col_num = models.IntegerField()
+    col_width = models.IntegerField()
+    col_width_type = CharField(max_length=2)
