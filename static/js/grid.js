80dd3f0d454ac8e18e2b4755c7726b7272bed56b
Sam Quayle <sam@erskinedesign.com>
2011-08-25 13:13:06 +0100

    Added header, grid overlay, typekit

---
+++ static/js/grid.js
@@ -0,0 +1,20 @@
+var image_src = 'http://localhost:8000/static/images/dev/grid.png',
+		$element = $('body'),
+		image = new Image();
+		
+	image.onload = function() {
+	    // always called
+	    var position = $element.position(),
+	    	$grid_overlay = $('<div id="grid" style="background:url(' + image_src + ') repeat-y 0 0; display: none; position: fixed; margin: 0; z-index: 999; height: 100%";/>');
+	    $grid_overlay.width($element.innerWidth());
+	    $grid_overlay.css({ 'left': position.left, 'top': position.top });
+	    	
+	    $('body').prepend($grid_overlay);
+		$(document).keyup(function(evt) {
+			if (evt.keyCode == 71) {
+				$grid_overlay.toggle();
+			}
+		});	
+	};
+	image.src = image_src;
+
