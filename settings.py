d6d02823b6d2caf57c55a583771aa12b1c645c72
Wil Linssen <wil@erskinedesign.com>
2011-08-28 09:22:18 +0100

    fixed the graduate model and added it to installed apps

--- settings.py
+++ settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django.contrib.webdesign',
+    'gridulator.gridulate',
 )
 
 # A sample logging configuration. The only tangible logging
