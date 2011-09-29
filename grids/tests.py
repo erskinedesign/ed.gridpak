4cfceb400676c3c500e09f591d1be0a7c16f7efc
Wil Linssen <wil@erskinedesign.com>
2011-09-29 15:14:53 +0100

    first test for grids - index is a 200

--- grids/tests.py
+++ grids/tests.py
@@ -1,16 +1,9 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GridsViewsTestCase(TestCase):
+
+    def test_index(self):
+        resp = self.client.get(reverse('grids_index'))
+        self.assertEqual(resp.status_code, 200)
