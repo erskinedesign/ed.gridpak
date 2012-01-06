cd9d310baadb131bf2e13909e2ea36bc88386fe4
Wil Linssen <wil@erskinedesign.com>
2012-01-06 16:38:05 +0000

    range is now 1 to the limit

--- grids/templatetags/common.py
+++ grids/templatetags/common.py
@@ -21,7 +21,7 @@ def get_range(value):
 
     Instead of 3 one may use the variable set in the views
   """
-  return range(1, value + 1)
+  return range(1, value)
 
 @register.filter
 def mult(value, arg):
