770d8a0a7d88b038c36d7cd5054545dde820bec0
Wil Linssen <wil@erskinedesign.com>
2012-01-06 12:25:06 +0000

    minusing too many gutters

--- static/js/gridpak.js
+++ static/js/gridpak.js
@@ -511,7 +511,7 @@ $(function() {
                 gutter_width = parseFloat($('#new_gutter_width').val()),
                 gutter_type = $('input[name="gutter_type"]:checked').val(),
                 // baseline_height = parseInt($('#new_baseline_height').val()),
-                gutter_remove = gutter_type == '%' ? gutter_width * col_num : 0,
+                gutter_remove = (gutter_type == '%') ? gutter_width * (col_num - 1) : 0,
                 col_width = (100 - gutter_remove) / col_num;
             return {
                 col_num: col_num,
