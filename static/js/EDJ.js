8bbbd8e09629458c474e67d13bfdce49f25952ea
Wil Linssen <wil@erskinedesign.com>
2012-01-16 17:03:12 +0000

    updated the EDJ moved unload stuff into EDJ

--- static/js/EDJ.js
+++ static/js/EDJ.js
@@ -1,67 +1,7 @@
-// Set up our global Erskine Design Javascript object
-var EDJ = {
-    settings: {
-        debug: true,
-        STATIC_URL: '/static/'
-    },
-    is_touch: false,
-    run_list: [
-        'navigation'
-    ],
-    $body: {}
-};
-
-
-/**
- * Bootstrap
- *
- * Called immediately - blocking script, so be careful
- *
- * Used on: all pages
- *
- * @namespace EDJ
- * @class init
- */
-EDJ.bootstrap = {
-
-    /**
-     * Is touch device
-     *
-     * returns true if the agent is a touch device
-     *
-     * @method is_touch_device
-     *
-     */
-    is_touch_device: function() {
-        try {
-            document.createEvent("TouchEvent");
-            return true;
-        } catch (e) {
-            return false;
-        }
-    },
-
-    /**
-     * Is JavaScript enabled
-     * 
-     * replaces the js-disabled class in the html tag with js-enabled for styling etc.
-     * @method is_javascript_enabled
-     *
-     */
-    is_javascript_enabled: function() {
-        var docElement = window.document.documentElement;
-        docElement.className = docElement.className.replace(/\bjs-disabled\b/,'') + ' js-enabled';
-    },
-
-    init: function() {
-        EDJ.is_touch = this.is_touch_device();
-        this.is_javascript_enabled();
-    }
-
-};
-
-// Run the bootstrap
-EDJ.bootstrap.init();
+EDJ.run_list = [
+    'navigation',
+    'glow_btn'
+];
 
 
 /**
@@ -157,7 +97,33 @@ EDJ.navigation = {
 
 };
 
-$('.presentational').delegate('.show_hide', 'click', function(){
-    console.log('here');
-    $(this).next('tr').toggleClass('hidden');
-});
+//$('.presentational').delegate('.show_hide', 'click', function(){
+    //console.log('here');
+    //$(this).next('tr').toggleClass('hidden');
+//});
+
+/**
+ * Glow button
+ *
+ * When click a glow button, it glows (natch)
+ *
+ */
+EDJ.glow_btn = {
+    $btn: {},
+    
+    run: function() {
+        this.$btn = $('#save_grid');
+        return this.$btn.length > 0;
+    },
+
+    init: function() {
+        this.$btn.live('click', function() {
+            $('#download_form')
+                .addClass('added')
+                .delay(2000)
+                .queue(function() {
+                    $(this).removeClass('added')
+               });
+        });
+    }
+};
