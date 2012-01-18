cd009bcc5ddbd8cfa85404dda2fd98e22acf6c16
Matt Smith <me@qbyt.me.uk>
2012-01-18 17:44:55 +0000

    Installed compressor

--- settings.py
+++ settings.py
@@ -80,6 +80,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -115,6 +116,7 @@ INSTALLED_APPS = (
     # 'django.contrib.sites',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
